1e85263f67fb012850235e8b84f3132600b3be0f
Abdulhamid <aeybeehamid@gmail.com>
2026-08-05 22:48:34 -0800

    Add finger state detection (up/down) for all five fingers

--- hand_tracking.py
+++ hand_tracking.py
@@ -16,9 +16,36 @@ while True:
 
     imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
+
+    lmList = []
+
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+
             mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        if lmList[4][1] > lmList[2][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        tipIds = [8, 12, 16, 20]
+        pipIds = [6, 10, 14, 18]
+
+        for i in range(4):
+            if lmList[tipIds[i]][2] < lmList[pipIds[i]][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        print(fingers)
 
     cv2.imshow("A.R.I.S - Hand Tracking", frame)
 
